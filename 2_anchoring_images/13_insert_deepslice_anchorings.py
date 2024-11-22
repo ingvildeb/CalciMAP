e25488513154c17c52b0224a5bc398a90389606b
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-11-22 13:33:48 +0100

    Adding deepslice scripts

---
+++ 2_anchoring_images/13_insert_deepslice_anchorings.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import pandas as pd
+
+# Import from module in parent directory
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+brain_section_scripts_path = os.path.join(parent_dir, 'brain_section_scripts')
+sys.path.insert(0, brain_section_scripts_path)
+
+import 
