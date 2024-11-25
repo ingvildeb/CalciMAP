e523f6ca2f6af1fbc084e60660c1d0988b063cc4
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-11-25 14:48:28 +0100

    Update 03_generate_quicknii_files.py

--- 2_anchoring_images/03_generate_quicknii_files.py
+++ 2_anchoring_images/03_generate_quicknii_files.py
@@ -13,30 +13,30 @@ sys.path.insert(0, brain_section_scripts_path)
 
 import alignment_json_utils as aju
 
-# List the IDs and markers with files to be copied
+# List the IDs and markers with pngs to be made into QuickNII json
 ids = [124]
-markers = ["cresyl_violet"]
 
 # Path to Excel sheet listing all animal IDs with metadata
 metadata = r"Y:\2021_Bjerke_DevMouse_projects\03_METADATA//animals_and_stains.xlsx"
 
 # Read and filter Excel sheet to the selected IDs and markers
 subjects = pd.read_excel(metadata)    
-subjects_filtered = subjects.loc[(subjects['id'].isin(ids)) & (subjects['marker'].isin(markers))] 
+subjects_filtered = subjects.loc[(subjects['id'].isin(ids))] 
 
 # Listing the metadata variables from the Excel sheet that are going to be used in our folder names
 ID = subjects_filtered["id"]
-marker = subjects_filtered["marker"]
 age = subjects_filtered["age"]
+ID_age_dict = dict(zip(ID,age))
 
-marker_shortnames = {"parvalbumin":"parv", "calbindin":"calb", "cresyl_violet":"CV"}
+ID = list(set(ID))
+
 template_ages = [4,7,14,21,28]
 
 # The base path for all subfolders
-base_path = r"Y:\2021_Bjerke_DevMouse_projects\01_DATA\QuickNII_registration_workspace\\"
+base_path = r"Y:\2021_Bjerke_DevMouse_projects\QuickNII_registration_workspace\\"
 
-
-for i, m, a in zip(ID, marker, age):
+for i in ID:
+    a = ID_age_dict.get(i)
 
     if a in template_ages:
         template = "template"
@@ -45,15 +45,20 @@ for i, m, a in zip(ID, marker, age):
     
     print(i, a, template)
     
-    files_path = rf"{basePath}P{a}\Mouse{i}\\"
-    sorted_slices_dicts = aju.create_quicknii_slicedict(files_path)
-    
-    if a == "120":
-        json_dict = {"name":f"{ID}_jointAnchoring","target":"ABA_Mouse_CCFv3_2017_25um.cutlas","target-resolution":[428,524,320],"slices":sorted_slices_dicts}
+    files_path = rf"{base_path}P{a}\Mouse{i}\\"
+    name = f"Mouse{i}_TESTTESTjointAnchoring"
 
-    else:                                            
-        json_dict = {"name":f"{ID}_jointAnchoring","target":f"DeMBAv2_{a}_{template}.cutlas","target-resolution":[570,705,400],"slices":sorted_slices_dicts}
-    
-    
-    with open(rf"{basePath}P{a}\Mouse{i}\Mouse{i}_joint.json", "w") as outfile:
-        json.dump(json_dict, outfile)   
+    if a == "120":
+        target = "ABA_Mouse_CCFv3_2017_25um.cutlas"
+        target_resolution = [428,524,320]
+
+    else:        
+        target = f"DeMBAv2_P{a}_{template}.cutlas"
+        target_resolution = [570,705,400]   
+
+    aju.create_quicknii_slicedict(files_path, 
+                                  rf"{files_path}{name}",
+                                  name,
+                                  target,
+                                  target_resolution)                  
+        
