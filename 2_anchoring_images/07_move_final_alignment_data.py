9cc66b9424bbb8db47988eca1e10b74aa4323ed3
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-12-10 19:37:36 +0100

    Update 07_move_final_alignment_data.py

--- 2_anchoring_images/07_move_final_alignment_data.py
+++ 2_anchoring_images/07_move_final_alignment_data.py
@@ -3,15 +3,14 @@ import shutil
 import pandas as pd
 
 # List the IDs and markers with files to be renamed
-ids = [110]
-markers = ["calbindin"]
+markers = ["calbindin", "parvalbumin"]
 
 # Path to Excel sheet listing all animal IDs with metadata
 metadata = r"Y:\2021_Bjerke_DevMouse_projects\03_METADATA//animals_and_stains.xlsx"
 
 # Read and filter Excel sheet to the selected IDs and markers
 subjects = pd.read_excel(metadata)    
-subjects_filtered = subjects.loc[(subjects['id'].isin(ids)) & (subjects['marker'].isin(markers))] 
+subjects_filtered = subjects.loc[subjects['marker'].isin(markers)] 
 
 # Listing the metadata variables from the Excel sheet that are going to be used in our folder names
 ID = subjects_filtered["id"]
@@ -22,6 +21,7 @@ sex = subjects_filtered["sex"]
 marker_shortnames = {"parvalbumin":"parv", "calbindin":"calb", "cresyl_violet":"CV"}
 
 for i, m, a, s in zip(ID, marker, age, sex):
+    print(i,m)
     marker_short = marker_shortnames.get(m)
     file_base_path = rf"Y:\2021_Bjerke_DevMouse_projects\01_DATA\P{a}\{m.capitalize()}\Mouse{i}\\"
     final_anchoring_path = fr"{file_base_path}/thumbnails_for_anchoring//"
