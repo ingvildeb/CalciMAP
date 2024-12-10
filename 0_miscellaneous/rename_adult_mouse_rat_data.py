fa06cdb9b413d24528670c62237b59d537a195e2
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-12-10 20:55:59 +0100

    Update rename_adult_mouse_rat_data.py

--- 0_miscellaneous/rename_adult_mouse_rat_data.py
+++ 0_miscellaneous/rename_adult_mouse_rat_data.py
@@ -15,21 +15,45 @@ ids_species = {81264:"Mouse",81265:"Mouse",81266:"Mouse",81267:"Mouse",25205:"Ra
 markers_startnr = {"parvalbumin":3, "nissl":1}
 
 markers = ["parvalbumin"]
-ids = [81265]
+ids = [25206]
 
 check_only = True
 
 for m in markers:
     for i in ids:
         species = ids_species.get(i)
-        tiff_dir = rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\{m.capitalize()}\Mouse{i}\2_TIF\\"
+        startnr = markers_startnr.get(m)
 
-        renumber_dict = fnf.sequential_to_real_sections(tiff_dir,3,3)
+        file_base_path = rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\{m.capitalize()}\{species}{i}\\"
+        tiff_dir = rf"{file_base_path}2_TIF\\"
+        thumbs_dir = rf"{file_base_path}thumbnails\\"
+        anchoring_dir = rf"{file_base_path}thumbnails_for_anchoring\\"
+        anchoring_working_dir = rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\Anchoring_workspace\{species}{i}\\"
         
-        if check_only == True:
-            print(renumber_dict)
-        else:
-            continue
-            #fnf.exchange_sequential_sections(tiff_dir,renumber_dict)
+        dirs = [tiff_dir, thumbs_dir, anchoring_dir, anchoring_working_dir]
+        dirs_extension = {tiff_dir: ".tif", thumbs_dir: "_thumbnail.png", 
+                          anchoring_dir: "_thumbnail.png", anchoring_working_dir: "_thumbnail.png"}
+        dirs_json = {tiff_dir: None, thumbs_dir: None, 
+                          anchoring_dir: None, 
+                          anchoring_working_dir: rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\Anchoring_workspace\{species}{i}\{species}{i}_{m}_Anchoring.json"}
+
+        for dir in dirs:
+            extension = dirs_extension.get(dir)
+            print(f"{dir}") 
+            print(len(glob.glob(f"{dir}*{extension}")))
+            renumber_dict = fnf.sequential_to_real_sections(dir,startnr,3,extension)
+            json_file = dirs_json.get(dir)
+        
+            if check_only == True:
+                print(renumber_dict)
+                if json_file:
+                    print(f"Looking for {json_file}")
+                    if os.path.exists(json_file):
+                        print("The JSON file exists.")
+                    else:
+                        print("The JSON file does not exist.")
+            else:
+                print(f"Renumbering {dir}...")
+                fnf.exchange_sequential_sections(dir,renumber_dict,json_file,extension)
 
 
