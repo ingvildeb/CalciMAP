2733978ae5f65431557a19832ea0c8bbd5cab15c
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-12-11 20:01:28 +0100

    Update get_project_stats.py

--- 0_miscellaneous/get_project_stats.py
+++ 0_miscellaneous/get_project_stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import glob
+import os
 
 # Reading the metadata
 metadata = r"Y:\2021_Bjerke_DevMouse_projects\03_METADATA\Animal metadata.xlsx"
@@ -11,24 +12,41 @@ markers = ["calbindin", "parvalbumin", "cresyl_violet"]
 
 subjects = pd.read_excel(id_file)
 save_dir = r"Y:\2021_Bjerke_DevMouse_projects\03_METADATA//"
-
+extra_ids = [81264, 81265, 81266, 81267]
+ages = [9,14,21,35,120]
 
 for m in markers:
-    subjects_filtered = subjects.loc[subjects['marker'] == m]
-    ID = subjects_filtered["id"]
-    marker = subjects_filtered["marker"]
-    age = subjects_filtered["age"]
-    sex = subjects_filtered["sex"]
-
+    subjects_filtered = subjects.loc[(subjects['marker'] == m) & subjects['age'].isin(ages)]
+    ids = subjects_filtered["id"].tolist()
+    ids.extend(extra_ids)
+    subjects_filtered = subjects_filtered.set_index('id')
     list_of_data = []
-    for i, m, a, s in zip(ID, marker, age, sex):
-        tif_dir = fr"Y:/2021_Bjerke_DevMouse_projects/01_DATA/P{a}/{m.capitalize()}/Mouse{i}/2_TIF/" 
-        tif_list = glob.glob(f"{tif_dir}*.tif")
-        number_of_images = len(tif_list)
-        data = pd.DataFrame(data = [i,m,a,s,number_of_images])
-        data = data.transpose()
-        list_of_data.append(data)
 
+    for i in ids:
+        if i in subjects_filtered.index:
+            #m = subjects_filtered.loc[i,"marker"]
+            a = subjects_filtered.loc[i,"age"]
+            s = subjects_filtered.loc[i,"sex"]
+            tif_dir = fr"Y:/2021_Bjerke_DevMouse_projects/01_DATA/P{a}/{m.capitalize()}/Mouse{i}/2_TIF/" 
+            
+        else:
+            a = 120
+            s = "M"
+
+            if m == "calbindin":
+                continue
+            elif m == "parvalbumin":
+                tif_dir = fr"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\Mouse\Parvalbumin/Mouse{i}\2_TIF//"
+            else:
+                tif_dir = fr"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\Mouse\Nissl/Mouse{i}\2_TIF//"
+        if os.path.exists(tif_dir):
+            tif_list = glob.glob(f"{tif_dir}*.tif")
+            number_of_images = len(tif_list)
+            data = pd.DataFrame(data = [i,m,a,s,number_of_images])
+            data = data.transpose()
+            list_of_data.append(data)
+        else:
+            continue
 
     concat_df = pd.concat(list_of_data)
     concat_df.columns = ["ID", "marker", "age", "sex", "number of tifs"]
