c96e1775fb3e833efeca8f84c67571cd52b9496f
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-12-17 12:34:49 +0100

    Update anchoring_adult_mouse_rat_data.py
    Added code to create anchoring files with both nissl and pv based on the existing pv anchorings

--- 0_miscellaneous/anchoring_adult_mouse_rat_data.py
+++ 0_miscellaneous/anchoring_adult_mouse_rat_data.py
@@ -2,6 +2,8 @@ import glob
 import os
 import re
 import sys
+import json
+import shutil
 
 # Import from module in parent directory
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,12 +14,11 @@ import alignment_json_utils as aju
 
 # List the IDs and markers with files to be renamed
 ids_species = {81264:"Mouse",81265:"Mouse",81266:"Mouse",81267:"Mouse",25205:"Rat",25206:"Rat",25203:"Rat",25204:"Rat"}
-markers_startnr = {"parvalbumin":3, "nissl":1}
-species_target = {"Mouse": "ABA_Mouse_CCFv3_2017_25um.cutlas", "Rat": "WHS_Rat_v4_02_39um.cutlas"}
+species_target = {"Mouse": "ABA_Mouse_CCFv3_2017_25um.cutlas", "Rat": "WHS_Rat_v4_39um.cutlas"}
 species_resolution = {"Mouse": [428,524,320], "Rat": [512.0, 1024.0, 512.0]}
 species_suffix = {"Mouse": "nonlin", "Rat": "WHSv4_nonlinear"}
 markers = ["parvalbumin"]
-ids = [81265, 81266, 81267]
+ids = [25203, 25204, 25205, 81264, 81265, 81266, 81267]
 
 check_only = True
 
@@ -45,3 +46,53 @@ for m in markers:
                                     target, 
                                     target_resolution,
                                     False)
+
+# Create anchoring files with nissl & pv
+
+for i in ids:
+        species = ids_species.get(i)
+        print(f"Preparing file for {species}{i}")
+        anchoring_dir = rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\Anchoring_workspace\{species}{i}\\"
+        target = species_target.get(species)
+        target_resolution = species_resolution.get(species)
+
+        aju.create_quicknii_slicedict(anchoring_dir, 
+                        rf"{anchoring_dir}{species}{i}_joint",
+                        rf"{species}{i}_joint",
+                        target,
+                        target_resolution)   
+        
+        aju.insert_existing_anchorings(rf"{anchoring_dir}{species}{i}_parvalbumin_Anchoring.json", 
+                                rf"{anchoring_dir}{species}{i}_joint.json", 
+                                rf"{anchoring_dir}{species}{i}_jointAnchoring",
+                                f"{species}{i}_Anchoring", 
+                                target, 
+                                target_resolution,
+                                True)
+        
+
+# Split jsons into nissl & parv separate files
+
+
+specific_strings = ["parv", "nissl"]
+string_markers = {"parv": "Parvalbumin", "nissl": "Nissl"}
+
+for i in ids:
+    species = ids_species.get(i)
+    anchoring_dir = rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\Anchoring_workspace\{species}{i}\\"
+    target = species_target.get(species)
+    json_file = rf"{anchoring_dir}{species}{i}_jointAnchoring_final_nonlinear.json"
+    json_data = aju.read_json(json_file)
+
+    for str in specific_strings:
+        marker = string_markers.get(str)
+        specific_dict = aju.split_json(json_data, str)
+        specific_json = aju.create_quicknii_json_dict(json_data["name"], target, json_data["target-resolution"])
+        specific_json["slices"] = specific_dict
+        final_anchoring_path = rf"Y:\NESYS_Active_projects\2019_Laja_Brainwide_Parvalbumin\{species}\{marker}\{species}{i}\thumbnails_for_anchoring\\"
+        
+        with open(rf"{anchoring_dir}{species}{i}_anchoring_{str}.json", "w") as outfile:
+            json.dump(specific_json, outfile)  
+
+        with open(f"{final_anchoring_path}{species}{i}_anchoring_{str}.json", "w") as outfile:
+            json.dump(specific_json, outfile)
